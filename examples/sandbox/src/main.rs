133cc071472e64a1150cc3fb61a1b88cb5477a94
Jannis <info.fluxxcode@gmail.com>
2024-10-02 23:16:26 +0200

    Add basic spinner

--- examples/sandbox/src/main.rs
+++ examples/sandbox/src/main.rs
@@ -1,5 +1,39 @@
-use egui_modal_spinner;
+use eframe::egui;
 
-fn main() {
-    println!("Hello, world! {}", egui_modal_spinner::add(20, 40));
+use egui_modal_spinner::ModalSpinner;
+
+struct MyApp {
+    spinner: ModalSpinner,
+}
+
+impl MyApp {
+    pub fn new() -> Self {
+        Self {
+            spinner: ModalSpinner::new(),
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui application");
+            egui::widgets::global_theme_preference_buttons(ui);
+
+            self.spinner.update(ctx);
+        });
+    }
+}
+
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([1080.0, 720.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "My egui application",
+        options,
+        Box::new(|_| Ok(Box::new(MyApp::new()))),
+    )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#[derive(Debug, Clone, PartialEq)]
+pub struct ModalSpinner {
+    fill_color: egui::Color32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ModalSpinner {
+    pub fn new() -> Self {
+        Self {
+            fill_color: egui::Color32::from_rgba_premultiplied(0, 0, 0, 120),
+        }
     }
+
+    pub fn update(&mut self, ctx: &egui::Context) {
+        let window_fill = ctx.style().visuals.window_fill;
+        let screen_rect = ctx.input(|i| i.screen_rect);
+
+        ctx.style_mut(|s| s.visuals.window_fill = self.fill_color);
+
+        let re = egui::Window::new("TODO: Add a proper ID!")
+            .interactable(false)
+            .title_bar(false)
+            .fixed_size(screen_rect.size())
+            .fixed_pos(screen_rect.left_top())
+            .show(ctx, |ui| {
+                ui.centered_and_justified(|ui| {
+                    ui.spinner();
+                });
+            });
+
+        if let Some(re) = re {
+            ctx.move_to_top(re.response.layer_id);
+        }
+
+        ctx.style_mut(|s| s.visuals.window_fill = window_fill);
+    }
+}
+
+/// This tests if the spinner is send and sync.
+#[cfg(test)]
+const fn test_prop<T: Send + Sync>() {}
+
+#[test]
+const fn test() {
+    test_prop::<ModalSpinner>();
 }
