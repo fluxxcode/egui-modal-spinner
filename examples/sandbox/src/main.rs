959362333d32e4e2514b6a75a69f3b8e47627078
Jannis <info.fluxxcode@gmail.com>
2024-10-04 22:42:49 +0200

    Implement spinner state

--- examples/sandbox/src/main.rs
+++ examples/sandbox/src/main.rs
@@ -1,15 +1,53 @@
+use std::thread;
+use std::sync::mpsc::{self, TryRecvError};
+
 use eframe::egui;
 
 use egui_modal_spinner::ModalSpinner;
 
+enum ThreadState {
+    Closed,
+}
+
 struct MyApp {
     spinner: ModalSpinner,
+    result_recv: Option<mpsc::Receiver<ThreadState>>,
 }
 
 impl MyApp {
     pub fn new() -> Self {
+
         Self {
             spinner: ModalSpinner::new(),
+            result_recv: None,
+        }
+    }
+
+    fn exec_task(&mut self) {
+        let (tx, rx) = mpsc::channel();
+        self.result_recv = Some(rx);
+
+        thread::spawn(move || {
+            thread::sleep(std::time::Duration::from_secs(4));
+            let _ = tx.send(ThreadState::Closed);
+        });
+    }
+
+    fn update_task_thread(&mut self) {
+        if let Some(rx) = &self.result_recv {
+            match rx.try_recv() {
+                Ok(state) => match state {
+                    ThreadState::Closed => {
+                        self.spinner.close();
+                        self.result_recv = None;
+                    },
+                },
+                Err(err) => if err == TryRecvError::Disconnected {
+                    self.spinner.close();
+                    self.result_recv = None;
+                    println!("thread ended unexpectedly");
+                }
+            }
         }
     }
 }
@@ -19,6 +57,14 @@ impl eframe::App for MyApp {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("My egui application");
             egui::widgets::global_theme_preference_buttons(ui);
+
+            if ui.button("Do someting resource heavy!").clicked() {
+                self.exec_task();
+
+                self.spinner.open();
+            }
+
+            self.update_task_thread();
 
             self.spinner.update(ctx);
         });
--- src/lib.rs
+++ src/lib.rs
@@ -1,54 +1,88 @@
+//! egui-modal-spinner
+#![warn(missing_docs)] // Let's keep the public API well documented!
+
 use egui::Widget;
 
-/// Wrapper above `egui::Spinner` to be able to customize trait implementations.
+/// Represents the state the spinner is currently in.
 #[derive(Debug, Clone, PartialEq)]
-struct Spinner {
-    size: Option<f32>,
-    color: Option<egui::Color32>,
+pub enum SpinnerState {
+    /// The spinner is currently closed and not visible.
+    Closed,
+    /// The spinner is currently open and user input is suppressed.
+    Open,
 }
 
-impl Default for Spinner {
-    fn default() -> Self {
-        Self {
-            size: None,
-            color: None,
-        }
-    }
-}
-
-impl Spinner {
-    fn update(&self, ui: &mut egui::Ui) -> egui::Response {
-        let mut spinner = egui::Spinner::new();
-
-        if let Some(size) = self.size {
-            spinner = spinner.size(size);
-        }
-
-        if let Some(color) = self.color {
-            spinner = spinner.color(color);
-        }
-
-        spinner.ui(ui)
-    }
-}
-
+/// Represents a spinner instance.
 #[derive(Debug, Clone, PartialEq)]
 pub struct ModalSpinner {
+    state: SpinnerState,
     id: String,
+
     fill_color: egui::Color32,
     spinner: Spinner,
 }
 
+/// Creation methods
 impl ModalSpinner {
+    /// Creates a new spinner instance.
     pub fn new() -> Self {
         Self {
+            state: SpinnerState::Closed,
             id: String::from("_modal_spinner"),
+
             fill_color: egui::Color32::from_rgba_premultiplied(0, 0, 0, 120),
             spinner: Spinner::default(),
         }
     }
 
+    /// Sets the fill color of the modal background.
+    pub fn fill_color(mut self, color: impl Into<egui::Color32>) -> Self {
+        self.fill_color = color.into();
+        self
+    }
+
+    /// Sets the size of the spinner.
+    pub fn spinner_size(mut self, size: f32) -> Self {
+        self.spinner.size = Some(size);
+        self
+    }
+
+    /// Sets the color of the spinner.
+    pub fn spinner_color(mut self, color: impl Into<egui::Color32>) -> Self {
+        self.spinner.color = Some(color.into());
+        self
+    }
+}
+
+/// Getter and setter
+impl ModalSpinner {
+    /// Gets the current state of the spinner.
+    pub fn state(&self) -> &SpinnerState {
+        &self.state
+    }
+}
+
+/// Implementation methods
+impl ModalSpinner {
+    /// Opens the spinner.
+    pub fn open(&mut self) {
+       self.state = SpinnerState::Open;
+    }
+
+    /// Closes the spinner.
+    pub fn close(&mut self) {
+        self.state = SpinnerState::Closed;
+    }
+
+    /// Main update method of the spinner that should be called every frame if you want the
+    /// spinner to be visible.
+    ///
+    /// This has no effect if the `SpinnerState` is currently not `SpinnerState::Open`.
     pub fn update(&mut self, ctx: &egui::Context) {
+        if self.state != SpinnerState::Open {
+            return;
+        }
+
         let window_fill = ctx.style().visuals.window_fill;
         let screen_rect = ctx.input(|i| i.screen_rect);
 
@@ -80,4 +114,36 @@ const fn test_prop<T: Send + Sync>() {}
 #[test]
 const fn test() {
     test_prop::<ModalSpinner>();
+}
+
+/// Wrapper above `egui::Spinner` to be able to customize trait implementations.
+#[derive(Debug, Clone, PartialEq)]
+struct Spinner {
+    pub size: Option<f32>,
+    pub color: Option<egui::Color32>,
+}
+
+impl Default for Spinner {
+    fn default() -> Self {
+        Self {
+            size: None,
+            color: None,
+        }
+    }
+}
+
+impl Spinner {
+    fn update(&self, ui: &mut egui::Ui) -> egui::Response {
+        let mut spinner = egui::Spinner::new();
+
+        if let Some(size) = self.size {
+            spinner = spinner.size(size);
+        }
+
+        if let Some(color) = self.color {
+            spinner = spinner.color(color);
+        }
+
+        spinner.ui(ui)
+    }
 }
